7401e9c58f4358b85350d5184b04fd2afa58414e
sahian-0909 <al221910415@gmail.com>
2023-04-16 04:51:59 -0600

    init commit

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // spatie documentation
+        app()->make(\Spatie\Permission\PermissionRegistrar::class)->forgetCachedPermissions();
+
+        $permissions = [
+            'permission_index',
+            'permission_create',
+            'permission_show',
+            'permission_edit',
+            'permission_destroy',
+
+            'role_index',
+            'role_create',
+            'role_show',
+            'role_edit',
+            'role_destroy',
+
+            'user_index',
+            'user_create',
+            'user_show',
+            'user_edit',
+            'user_destroy',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create([
+                'name' => $permission
+            ]);
+        }
+    }
+}
---
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -0,0 +1,46 @@
+<div class="sidebar" data-color="purple" data-background-color="black">
+  <!--
+      Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
+
+      Tip 2: you can also add an image using data-image tag
+  -->
+  <div class="logo">
+    <a href="{{ route('home') }}" class="simple-text logo-normal">
+      Detto
+    </a>
+  </div>
+  <div class="sidebar-wrapper">
+    <ul class="nav">
+      <li class="nav-item{{ $activePage == 'dashboard' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('home') }}">
+          <i class="material-icons">dashboard</i>
+            <p>{{ __('Dashboard') }}</p>
+        </a>
+      </li>
+      @can('user_index')
+      <li class="nav-item{{ $activePage == 'users' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('users.index') }}">
+          <i class="material-icons">content_paste</i>
+            <p>Usuarios</p>
+        </a>
+      </li>
+      @endcan
+      @can('permission_index')
+      <li class="nav-item{{ $activePage == 'permissions' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('permissions.index') }}">
+          <i class="material-icons">bubble_chart</i>
+          <p>{{ __('Permissions') }}</p>
+        </a>
+      </li>
+      @endcan
+      @can('role_index')
+      <li class="nav-item{{ $activePage == 'roles' ? ' active' : '' }}">
+        <a class="nav-link" href="{{ route('roles.index') }}">
+          <i class="material-icons">location_ons</i>
+            <p>{{ __('Roles') }}</p>
+        </a>
+      </li>
+      @endcan
+    </ul>
+  </div>
+</div>
